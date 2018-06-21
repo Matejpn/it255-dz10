0b8dfe52d0fd736b88090a507b62f934208bece3
Matej <matejp91@gmail.com>
2018-06-21 19:28:30 +0200

    first commit

--- IT255-DZ10-master/it255-dz10.sql
+++ IT255-DZ10-master/it255-dz10.sql
@@ -1,14 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.1
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: May 18, 2017 at 05:22 PM
--- Server version: 5.6.34
--- PHP Version: 7.1.0
+-- Host: 127.0.0.1
+-- Generation Time: Jun 21, 2018 at 07:25 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `it255-dz10`
@@ -57,3 +65,8 @@ ALTER TABLE `meni`
 --
 ALTER TABLE `meni`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
